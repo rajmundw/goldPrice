2c326cd73181de1e5e8c8565e5288114f69142e0
Rajmund Waszkiel <rajmund.wu@gmail.com>
2018-12-20 20:38:47 +0100

    Updates

---
+++ precache-manifest.f80c7041f903c71a8a6f53da0c3e6689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d60b5353c92c17b6808",
+    "url": "/goldPrice/static/js/runtime~main.6d60b535.js"
+  },
+  {
+    "revision": "ca61818428a743e41663",
+    "url": "/goldPrice/static/js/main.ca618184.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/goldPrice/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "ca61818428a743e41663",
+    "url": "/goldPrice/static/css/main.be5bcfd3.chunk.css"
+  },
+  {
+    "revision": "cda15850df7e86259392c407dfd26003",
+    "url": "/goldPrice/index.html"
+  }
+];
